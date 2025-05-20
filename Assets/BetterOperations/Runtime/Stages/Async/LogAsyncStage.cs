d59b18624324d75cb28a298c52e9c46e820923c7
Anatolii Bryzhan <anatolii.bryzhan@gmail.com>
2025-05-20 15:33:34 +0200

    Add LogStages

---
+++ Assets/BetterOperations/Runtime/Stages/Async/LogAsyncStage.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Better.Operations.Runtime.Buffers;
+using Better.Operations.Runtime.Members;
+using UnityEngine;
+
+namespace Better.Operations.Runtime.Stages
+{
+    public class LogAsyncStage<TBuffer, TMember> : AsyncStage<TBuffer, TMember>
+        where TBuffer : AsyncBuffer<TMember>
+        where TMember : IOperationMember
+    {
+        protected LogType LogType { get; }
+        protected string OriginMessage { get; }
+
+        public LogAsyncStage(string message, LogType logType)
+        {
+            OriginMessage = message;
+            LogType = logType;
+        }
+
+        public override Task<bool> TryExecuteAsync(TBuffer buffer)
+        {
+            var message = $"{OriginMessage}\n{nameof(buffer)}:{buffer}";
+            Debug.unityLogger.Log(LogType, message);
+
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ Assets/BetterOperations/Runtime/Stages/Sync/LogSyncStage.cs
@@ -0,0 +1,30 @@
+﻿using Better.Operations.Runtime.Buffers;
+using Better.Operations.Runtime.Extensions;
+using Better.Operations.Runtime.Members;
+using Better.Operations.Runtime.Permissions;
+using UnityEngine;
+
+namespace Better.Operations.Runtime.Stages
+{
+    public class LogSyncStage<TBuffer, TMember> : SyncStage<TBuffer, TMember>
+        where TBuffer : SyncBuffer<TMember>
+        where TMember : IOperationMember
+    {
+        protected LogType LogType { get; }
+        protected string OriginMessage { get; }
+
+        public LogSyncStage(string message, LogType logType)
+        {
+            OriginMessage = message;
+            LogType = logType;
+        }
+
+        public sealed override bool TryExecute(TBuffer buffer)
+        {
+            var message = $"{OriginMessage}\n{nameof(buffer)}:{buffer}";
+            Debug.unityLogger.Log(LogType, message);
+
+            return true;
+        }
+    }
+}
